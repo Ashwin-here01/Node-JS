f2f3f488fec57734250aa16cdfb77ebe2b0b0c19
Ashwin-here01 <ashwin.online01@gmail.com>
2026-09-02 16:29:28 +0530

    Added a practice file for Practice

---
+++ Practice/Practice.js
@@ -0,0 +1,29 @@
+const file = require("fs");
+
+/*
+const {sum, sub, mul, div} = require("./mathFn");
+
+// console.log(math);
+
+console.log(sum(5, 5));
+
+// file.mkdirSync("./TextFiles");
+
+file.writeFileSync("./TextFiles/01.txt", "This is for practice");
+// setTimeout(() => {
+//     file.appendFileSync("./TextFiles/01.txt", "! And my name is Harry");
+// }, 3000);
+
+const result = file.readFileSync("./TextFiles/01.txt", "utf-8");
+console.log(result);
+
+file.readFile("./TextFiles/01.txt", "utf-8", (err, result) => {
+    if(err) console.log("Error: ", err);
+    else console.log(result);
+});
+
+// file.copyFileSync("./TextFiles/01.txt", "./TextFiles/02CopiedFile.txt");
+
+// file.unlinkSync("./TextFiles/02CopiedFile.txt");
+
+*/
---
+++ Practice/mathFn.js
@@ -0,0 +1,25 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+module.exports = {
+    sum,
+    sub,
+    mul,
+    div
+}
+
+exports.add = (a, b) => a + b;
+exports.modulo = (a, b) => a % b;
